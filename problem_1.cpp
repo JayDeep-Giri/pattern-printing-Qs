7e67f3a904f7b5521fa7f7ccb3b90c345fa9be3c
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-11-25 15:22:31 +0530

    Star Triangle Reversed

---
+++ problem_1.cpp
@@ -0,0 +1,24 @@
+// Star Triangle Reversed  
+/*
+        ******
+        *****
+        ****
+        ***
+        **
+        *
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+        int m,n;
+        cin>>m>>n;
+
+        for(int i=1; i<=m; i++){
+                for(int j=1; j<=n+1-i; j++){
+                        cout<<"*";
+                }
+                cout<<endl;
+        }
+
+}
